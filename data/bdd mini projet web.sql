c91b804efda799784c8bdbf9f84d2794187190ad
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-02-15 21:46:38 +0100

    Update SQL file

--- data/bdd mini projet web.sql
+++ data/bdd mini projet web.sql
@@ -3,6 +3,7 @@
 --# Author: 
 --# Function: Création schéma BD
 --###############################################################
+-- If tables already exist
 DROP TABLE IF EXISTS Personne;
 
 DROP TABLE IF EXISTS Signe;
@@ -23,7 +24,8 @@ CREATE TABLE Signe (
             'Verseau',
             'Poisson'
         )
-    ) PRIMARY KEY date_debut DATE,
+    ) PRIMARY KEY,
+    date_debut DATE,
     date_fin DATE,
     element VARCHAR(10) CONSTRAINT dom_element CHECK(element IN ('Air', 'Terre', 'Eau', 'Feu'))
 );
@@ -35,3 +37,8 @@ CREATE TABLE Personne (
     email VARCHAR(40),
     signe_astro VARCHAR(10) CONSTRAINT cle_etr_Personne FOREIGN KEY Signe(signe_astro)
 );
+
+----------- mettre le nom de la base de données ------------------
+GRANT all privileges ON Personne TO -----* * * * *-------;
+
+GRANT all privileges ON Signe TO -----* * * * *-------;
