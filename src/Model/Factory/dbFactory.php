e6470ec9c217ba9de0d07d450ac61ba8c57b9039
constant-gayet <gayet.constant@gmail.com>
2021-02-17 00:28:27 +0100

    connexion bdd 1

--- src/Model/Factory/dbFactory.php
+++ src/Model/Factory/dbFactory.php
@@ -5,6 +5,6 @@ namespace Rediite\Model\Factory;
 class dbFactory {
 
   function createService() {
-    return new \PDO('pgsql:dbname=tpphp;host=pgsql2;port=5432"', 'tpphp', 'tpphp');
+    return new \PDO('pgsql:dbname=ipw_alexia_harivel;host=pgsql2;port=5432"', 'ipw_alexia_harivel', 'ipw_alexia_harivel');
   }
 }
