a5310da3ea60d28396880fa1037ba9379c59afd7
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-02-18 13:13:04 +0100

    send  email

--- src/View/person.php
+++ src/View/person.php
@@ -14,3 +14,14 @@
   loadHoroscope($data->getAstrosign());
   ?>
 </div>
+
+<?php
+     $to      = $data->getEMail();
+     $subject = 'Merci !';
+     $message = 'Merci d\'avoir participer à notre formulaire ! Nous espérons vous revoir très vite sur notre site http://pgsql.pedago.ensiie.fr/~barnabe.geffroy/mini-projet/public/ :)';
+     $headers = 'From: no-reply@astrologiie.ensiie' . "\r\n" .
+     'Reply-To: no-reply@astrologiie.ensiie' . "\r\n" .
+     'X-Mailer: PHP/' . phpversion();
+
+     mail($to, $subject, $message, $headers);
+ ?>
