c4a219ebe06c1ec0731412b4dc80c83d539985e4
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-02-19 22:45:54 +0100

    Details...

--- src/Model/Repository/PersonRepository.php
+++ src/Model/Repository/PersonRepository.php
@@ -48,8 +48,8 @@ class PersonRepository
               ELSE (mois_debut * 100 + jour_debut) <= (:mounth * 100 + :jour) OR (:mounth * 100 + :jour) <= (mois_fin * 100 + jour_fin) 
           END'
     );
-    $request->bindValue(':jour', $day, \PDO::PARAM_STR);
-    $request->bindValue(':mounth', $mounth, \PDO::PARAM_STR);
+    $request->bindValue(':jour', $day, \PDO::PARAM_INT);
+    $request->bindValue(':mounth', $mounth, \PDO::PARAM_INT);
     $request->execute();
 
     $person->setZodiacSign($request->fetch());
