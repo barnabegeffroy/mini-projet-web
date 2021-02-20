75ee9a582266bc74d9bedf3d6787fbafe188a808
Barnabé GEFFROY <barnabe.geffroy@ensiie.fr>
2021-02-20 19:12:40 +0100

    JS script

--- src/Assets/Script/script.js
+++ src/Assets/Script/script.js
@@ -1,23 +1,66 @@
-var email = document.getElementById("email");
+/* Personnalisation du message d'erreur pour le prénom */
+{
+    var prenom = document.getElementById("prenom");
+    prenom.oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("On veut connaître comment tu t'appelles");
+        }
+    };
+    prenom.oninput = function (e) {
+        e.target.setCustomValidity("");
+    };
+}
+/* Personnalisation du message d'erreur pour le nom */
+{
+    var nom = document.getElementById("nom");
+    nom.oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("On veut connaître ton nom !");
+        }
+    };
+    nom.oninput = function (e) {
+        e.target.setCustomValidity("");
+    };
+}
+/* Personnalisation du message d'erreur pour l'e-mail */
+{
+    var email = document.getElementById("email");
+    email.oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Il nous faut une adresse mail valide");
+        }
+    };
+    email.oninput = function (e) {
+        e.target.setCustomValidity("");
+    };
+}
+/* Personnalisation du message d'erreur pour la date et mise à jour du max à la date d'aujourd'hui */
+{
+    var date = document.getElementById("date");
+    date.oninvalid = function (e) {
+        e.target.setCustomValidity("");
+        if (!e.target.validity.valid) {
+            e.target.setCustomValidity("Il nous faut ta date d'anniversaire (tu peux te rajeunir de quelques années si tu veux ;) )");
+        }
+    };
+    date.oninput = function (e) {
+        e.target.setCustomValidity("");
+    };
 
-email.addEventListener("keyup", function (event) {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Cela ne ressemble pas un e-mail correct...");
-    } else {
-        email.setCustomValidity("");
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
     }
-});
+    if (mm < 10) {
+        mm = '0' + mm
+    }
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
- if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
-
-today = yyyy+'-'+mm+'-'+dd;
-document.getElementById("datefield").setAttribute("max", today);
+    today = yyyy + '-' + mm + '-' + dd;
+    date.setAttribute("max", today);
+}
--- src/View/home.php
+++ src/View/home.php
@@ -10,22 +10,22 @@ endif; ?>
 
 <form action="gestionSigne.php" method="post">
   <label for="prenom">Prénom :</label>
-  <input type="text" id="prenom" name="prenom" required="required"/><br>
+  <input type="text" id="prenom" name="prenom" required/><br>
   <label for="nom">Nom :</label>
-  <input type="text" id="nom" name="nom" required="required"/><br>
+  <input type="text" id="nom" name="nom" required/><br>
   <label for="email">Email :</label>
-  <input type="email" id="email" name="email" required="required"/><br>
+  <input type="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" required/><br>
   <label for="date">Date de naissance :</label>
-  <input type="date" id="datefield" name="date" required="required" value="16-02-2021" max="01-01-2021"/><br>
+  <input type="date" id="date" name="date" required value="16-02-2021" max="01-01-1970"/><br>
   <fieldset>
     <legend>Etes-vous un homme ou une femme?</legend>
     <label for="h">Homme</label>
-    <input type="radio" id="h" value="h" name="sex" required="required"><br>
+    <input type="radio" id="h" value="h" name="sex" required><br>
     <label for="f">Femme</label>
-    <input type="radio" id="f" value="f" name="sex" required="required"><br>
+    <input type="radio" id="f" value="f" name="sex" required><br>
     <label for="a">Autre</label>
-    <input type="radio" id="a" value="a" name="sex" required="required"><br>
+    <input type="radio" id="a" value="a" name="sex" required><br>
   </fieldset>
   <button type="submit">Envoyer</button>
-  <script src="../Assets/Script/script.js"></script>
+  <script src="../src/Assets/Script/script.js"></script>
 </form>
